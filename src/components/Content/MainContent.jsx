cf39aaf50c7ba217dfe0d3b97613aab4a860460b
kaznin <kaznin1994@mail.ru>
2021-07-19 10:14:18 +0300

    add redirect from '/' to '/all'

--- src/components/Content/MainContent.jsx
+++ src/components/Content/MainContent.jsx
@@ -1,10 +1,12 @@
 import PropTypes from "prop-types";
 import { Route } from "react-router";
+import { Redirect } from "react-router-dom";
 import FilmsGrid from "./FilmsGrid";
 
 const MainContent = () => {
     return (
         <main>
+            <Redirect exact from="/" to="/all" />
             <Route path="/all" render={ () => <FilmsGrid /> } />
             <Route path="/family" render={ () => <FilmsGrid /> } />
             <Route path="/action" render={ () => <FilmsGrid /> } />
