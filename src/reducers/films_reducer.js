9f67413c132546d63a3777e389e4ff1ad4eb28cf
kaznin <kaznin1994@mail.ru>
2021-07-19 18:26:27 +0300

    add new filed 'isLoaderShow' to state

--- src/reducers/films_reducer.js
+++ src/reducers/films_reducer.js
@@ -1,8 +1,11 @@
 import _ from "lodash";
 
-const CHANGE_FILMS_LIST = "CHANGE_FILMS_LIST"; 
+const CHANGE_FILMS_LIST = "CHANGE_FILMS_LIST";
+const SHOW_LOADER = "SHOW_LOADER";
+const HIDE_LOADER = "HIDE_LOADER";  
 
 const init_state = {
+    isLoaderShow: false,
     filmsList: []
 }
 
@@ -11,6 +14,10 @@ const films_reducer = (state=init_state, action) => {
 
     if (action.type === CHANGE_FILMS_LIST) {
         stateCopy.filmsList = action.data
+    } else if (action.type === SHOW_LOADER) {
+        stateCopy.isLoaderShow = true;
+    } else if (action.type === HIDE_LOADER) {
+        stateCopy.isLoaderShow = false;
     }
 
     return stateCopy;
@@ -21,5 +28,13 @@ export default films_reducer;
 export let changeFilmsList = (data) => ({
     type: CHANGE_FILMS_LIST,
     data
+})
+
+export let showLoader = () => ({
+    type: SHOW_LOADER
+})
+
+export let hideLoader = () => ({
+    type: HIDE_LOADER
 })
 
