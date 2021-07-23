9519909c9a6ed5c7746208712c0d0719885033e7
kaznin <kaznin1994@mail.ru>
2021-07-23 09:11:56 +0300

    add propTypes to ActionListForFilmCard

--- src/components/Forms/ActionListForFilmCard/ActionListForFilmCard.jsx
+++ src/components/Forms/ActionListForFilmCard/ActionListForFilmCard.jsx
@@ -2,11 +2,12 @@ import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { compose } from "redux";
 import { hideActionList } from "./../../../reducers/action_list_reducer";
+import PropTypes from "prop-types";
 import style from "./ActionListForFilmCard.module.css";
 
 const ActionListForFilmCard = (props) => {
-    let top = props.top;
-    let left = props.left;
+    let top = props.top-10; // -10 чтобы курсор был внутри ActionListForFilmCard
+    let left = props.left-10;
     let isActionListShow = props.isActionListShow;
 
     let hideActionList = props.hideActionList;
@@ -17,11 +18,12 @@ const ActionListForFilmCard = (props) => {
         if (actionList.current) {
             actionList.current.style.top = `${top}px`;
             actionList.current.style.left = `${left}px`; 
+            actionList.current.style.display = "inline-flex"; 
         }    
     }, [top, left, isActionListShow]);
 
     return isActionListShow && (
-        <ul ref={ actionList }  className={ style.actionList } onMouseLeave={ hideActionList }>
+        <ul ref={ actionList }  className={ style.actionList }  onMouseLeave={ hideActionList }>
             <li onClick={ hideActionList }>&#10006;</li>
             <li>Edit</li>
             <li>Delete</li>
@@ -41,3 +43,11 @@ let ActionListForFilmCardContainer = compose(
 )(ActionListForFilmCard)
 
 export default ActionListForFilmCardContainer;
+
+ActionListForFilmCard.propTypes = {
+    top: PropTypes.number,
+    left: PropTypes.number,
+    isActionListShow: PropTypes.bool,
+    film_id: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    hideActionList: PropTypes.func
+}
